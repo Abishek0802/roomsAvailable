3db4d3bb88b68b7e8791c08b1e584be0e816612f
Abishek0802 <abishek20td0802@gmail.com>
2023-12-22 15:19:40 +0530

    ....

--- Rooms/src/App.jsx
+++ Rooms/src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import Demo from "./Demo.jsx"
-import './App.css'
+
 
 function App() {
   const [count, setCount] = useState(0)
--- Rooms/src/Demo.jsx
+++ Rooms/src/Demo.jsx
@@ -1,24 +1,87 @@
-import React from "react";
+import { Box, Typography } from "@mui/material";
+import IconButton from "@mui/material/IconButton";
+import ArrowCircleLeftTwoToneIcon from "@mui/icons-material/ArrowCircleLeftTwoTone";
+const Boxstyle = {
+  height: "150px",
+  width: "100%",
+  margin: "50px auto",
+  borderRadius: "0 0 20px 20px ",
+  overflow: "hidden",
+  backgroundImage: "linear-gradient(to bottom, #030327, #19906F)",
+  position: "relative",
+};
+const circleRight = {
+  position: "absolute",
+  top: "50px",
+  right: "-50px",
+  backgroundColor: "rgba(255, 255, 255, 0.5)",
+  backdropFilter: "blur(33%)",
+  height: "300px",
+  width: "350px",
+  borderRadius: "50%",
+};
+const circleLeft = {
+  position: "absolute",
+  top: "-50px",
+  right: "80%",
+  backgroundColor: "rgba(255, 255, 255, 0.5)",
+  backdropFilter: "blur(33%)",
+  height: "150px",
+  width: "400px",
+  borderRadius: "60%",
+};
+const bigCircleLeft = {
+  position: "absolute",
+  top: "-40px",
+  right: "-70px",
+  backgroundColor: "rgba(255, 255, 255, 0.1)",
+  backdropFilter: "blur(33%)",
+  height: "350px",
+  width: "700px",
+  borderRadius: "60% 30%",
+};
+const bigCircleRight = {
+  position: "absolute",
+  top: "-20px",
+  right: "60%",
+  backgroundColor: "rgba(255, 255, 255, 0.1)",
+  backdropFilter: "blur(33%)",
+  height: "400px",
+  width: "600px",
+  borderRadius: "50%",
+};
+const topicContent = {
+  position: "absolute",
+  top: "25px",
+  left: "45px",
+};
+const Demo = () => {
 
-const MyMaterialUIComponent = () => {
-  const containerStyle = {
-    width: "100%",
-    height: "100%",
-    opacity: 0.4,
-    background: "#6B808E",
-    borderRadius: "9999px",
-  };
 
   return (
-    <div style={containerStyle}>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti quas
-      adipisci autem labore odio animi veniam vitae, ullam aliquam velit enim
-      nostrum? Numquam ea ex enim reprehenderit ut neque id! Lorem ipsum dolor,
-      sit amet consectetur adipisicing elit. Quaerat aspernatur ea et tempora
-      fugit alias voluptatibus corporis ex iusto nobis nihil perferendis
-      nesciunt eaque quas quasi repellat perspiciatis, explicabo eius!
-    </div>
+    <>
+      <Box sx={Boxstyle} position={'static'}>
+        <Box sx={bigCircleLeft}></Box>
+        <Box sx={bigCircleRight}></Box>
+        <Box sx={circleRight}></Box>
+        <Box sx={circleLeft}></Box>
+        <Box sx={topicContent}>
+          <Typography variant="h5" fontWeight={"bold"} color={"white"} mt={1} textTransform={'uppercase'}>
+            <IconButton
+              size="large"
+              edge="start"
+              color="inherit"
+              aria-label="menu"
+              sx={{ mr: 1 }}
+            >
+              <ArrowCircleLeftTwoToneIcon fontSize="large" />
+            </IconButton>
+            {"room"}
+          </Typography>
+        </Box>
+      </Box>
+    </>
   );
 };
 
-export default MyMaterialUIComponent;
+export default Demo;
