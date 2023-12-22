0110f34630ee3abc421ff4a8d26458ede0c7a82b
Abishek0802 <abishek20td0802@gmail.com>
2023-12-22 12:19:49 +0530

    ..

---
+++ Rooms/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import Demo from "./Demo.jsx"
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+
+   
+     <Demo/>
+    </>
+  )
+}
+
+export default App
---
+++ Rooms/src/Demo.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+const MyMaterialUIComponent = () => {
+  const containerStyle = {
+    width: "100%",
+    height: "100%",
+    opacity: 0.4,
+    background: "#6B808E",
+    borderRadius: "9999px",
+  };
+
+  return (
+    <div style={containerStyle}>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti quas
+      adipisci autem labore odio animi veniam vitae, ullam aliquam velit enim
+      nostrum? Numquam ea ex enim reprehenderit ut neque id! Lorem ipsum dolor,
+      sit amet consectetur adipisicing elit. Quaerat aspernatur ea et tempora
+      fugit alias voluptatibus corporis ex iusto nobis nihil perferendis
+      nesciunt eaque quas quasi repellat perspiciatis, explicabo eius!
+    </div>
+  );
+};
+
+export default MyMaterialUIComponent;
